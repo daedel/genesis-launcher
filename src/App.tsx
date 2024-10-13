4acff4e79e655140d542bc1d4be424c452587bfb
daedel <dgasiorowskii@gmail.com>
2024-10-14 00:29:08 +0200

    remove padding

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="bg-[#191b28] w-[715px]">
       {/* <div className="border-0 bg-contain bg-no-repeat bg-window_frame z-[9999] w-full"> */}
-        <div className="flex flex-col text-center h-screen w-full border-0 px-[0.18rem]">
+        <div className="flex flex-col text-center h-screen w-full border-0">
           <TopBar />
           <LoadingProvider>
             <Banner />
