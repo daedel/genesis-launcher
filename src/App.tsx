be034c537d494d012f11c59cbb770b4e227e030e
daedel <dgasiorowskii@gmail.com>
2024-10-14 18:11:11 +0200

    again fix :(

--- src/App.tsx
+++ src/App.tsx
@@ -9,53 +9,53 @@ import { LoadingProvider } from "./contexts/Loading";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import TopBar from "./components/TopBar";
 import { appWindow, LogicalSize } from '@tauri-apps/api/window';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
 
 
 
 function App() {
-  const divRef = useRef(null);
-  const [width, setWidth] = useState(0); // Stan do przechowywania szerokości
-  const [height, setHeight] = useState(0); // Stan do przechowywania szerokości
+  
+  let width = 0;
+  let height = 0;
 
+  // Function handling scale change (DPI)
+  const handleScaleChange = async (width: number, height: number) => {
+    // Zmiana rozmiaru okna na 715x505 po zmianie skali
+    console.log('Zmieniam rozmiar na width: ', width, ' height: ', height);
+    await appWindow.setSize(new LogicalSize(width,  height ));
+  };
+
+  const fixWindowSize = async () => {
+    console.log('Sprawdzam rozmiar diva');
+    const scaleFactor = await appWindow.scaleFactor();
+    const currentSize = (await appWindow.innerSize()).toLogical(scaleFactor);
+    console.log('currentSize: ', currentSize);
+    console.log('width: ',width, ' height: ', height);
+
+    const w_diff = currentSize.width - width;
+    const h_diff = currentSize.height - height;
+    if (w_diff > 0 && width > 0 || h_diff > 0 && height > 0) {
+      const scaleDiffWidth = (width * 100) /currentSize.width;
+      const scaleDiffHeight = (height * 100) /currentSize.height;
+      console.log('Roznica scaleDiffWidth: ', scaleDiffWidth);
+      console.log('Roznica scaleDiffHeight: ', scaleDiffHeight);
+      await handleScaleChange(currentSize.width + (currentSize.width * scaleDiffWidth), currentSize.height + (currentSize.height * scaleDiffHeight));
+    }
+  }
 
   useEffect(() => {
-    if (divRef.current) {
-      const element = divRef.current as HTMLDivElement;
-      setWidth(element.offsetWidth);
-      setHeight(element.offsetHeight);
+    const appContainer = document.getElementById('main-div');
+    if (appContainer) {
+      const dimensions = appContainer.getBoundingClientRect();
+      console.log('Width:', dimensions.width);
+      console.log('Height:', dimensions.height);
+      width = dimensions.width;
+      height = dimensions.height;
+    } else {
+      console.error('Element with id "main-div" not found');
     }
     
-    // Funkcja obsługująca zmianę skali (DPI)
-    const handleScaleChange = async (width: number, height: number) => {
-      // Zmiana rozmiaru okna na 715x505 po zmianie skali
-      console.log('Zmieniam rozmiar na width: ', width, ' height: ', height);
-      await appWindow.setSize(new LogicalSize(width,  height ));
-    };
-
-    const fixWindowSize = async () => {
-      console.log('Sprawdzam rozmiar diva');
-      const scaleFactor = await appWindow.scaleFactor();
-      const currentSize = (await appWindow.innerSize()).toLogical(scaleFactor);
-      console.log('currentSize: ', currentSize);
-      console.log('width: ',width, ' height: ', height);
-
-      const w_diff = currentSize.width - width;
-      const h_diff = currentSize.height - height;
-      if (w_diff > 0 && width > 0 || h_diff > 0 && height > 0) {
-        const scaleDiffWidth = (width * 100) /currentSize.width;
-        const scaleDiffHeight = (height * 100) /currentSize.height;
-        console.log('Roznica scaleDiffWidth: ', scaleDiffWidth);
-        console.log('Roznica scaleDiffHeight: ', scaleDiffHeight);
-        await handleScaleChange(currentSize.width + (currentSize.width * scaleDiffWidth), currentSize.height + (currentSize.height * scaleDiffHeight));
-      }
-    }
     fixWindowSize();
-    // Nasłuchiwanie na zdarzenie zmiany skali
-    // const unlisten = appWindow.onScaleChanged(({ payload }) => {
-    //   console.log('Skala zmieniona na: ', payload);
-    //   handleScaleChange();
-    // });
 
     // Czyszczenie event listenera po odmontowaniu komponentu
     return () => {
@@ -69,7 +69,7 @@ function App() {
   // console.log(`Skalowanie DPI: ${factor}`);
   // appWindow.setSize(new PhysicalSize(800 * factor, 600 * factor));
   return (
-    <div ref={divRef} className="bg-[#191b28] w-screen h-screen">
+    <div id="main-div" className="bg-[#191b28] w-screen h-screen">
       {/* <div className="border-0 bg-contain bg-no-repeat bg-window_frame z-[9999] w-full"> */}
       <div className="flex flex-col text-center h-screen w-full">
         <TopBar />
