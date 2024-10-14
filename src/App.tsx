bbd4076a1c228422d8750a11ca72c9ae8737fe73
daedel <dgasiorowskii@gmail.com>
2024-10-14 23:17:26 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -14,70 +14,71 @@ import { useEffect } from 'react';
 
 
 function App() {
-  
-  let width = 0;
-  let height = 0;
 
-  // Function handling scale change (DPI)
   const handleScaleChange = async (width: number, height: number) => {
     // Zmiana rozmiaru okna na 715x505 po zmianie skali
     console.log('Zmieniam rozmiar na width: ', width, ' height: ', height);
-    await appWindow.setSize(new LogicalSize(width,  height ));
+    await appWindow.setSize(new LogicalSize(width, height));
   };
 
-  const fixWindowSize = async () => {
-    console.log('Sprawdzam rozmiar diva');
-    const scaleFactor = await appWindow.scaleFactor();
-    const currentSize = (await appWindow.innerSize()).toLogical(scaleFactor);
-    console.log('currentSize: ', currentSize);
-    console.log('width: ',width, ' height: ', height);
-
-    const w_diff = currentSize.width - width;
-    const h_diff = currentSize.height - height;
-    if (w_diff > 0 && width > 0 || h_diff > 0 && height > 0) {
-      await handleScaleChange(currentSize.width + w_diff, currentSize.height + h_diff);
-    }
-  }
-
-  useEffect(() => {
+  const setCorrectWindowSize = async (targetWidth: number) => {
+    let iterations = 0;
     const appContainer = document.getElementById('main-div');
-    if (appContainer) {
-      const dimensions = appContainer.getBoundingClientRect();
+    if (!appContainer) {
+      return;
+    }
+    let dimensions = appContainer.getBoundingClientRect();
+    const width = dimensions.width;
+    // const height = dimensions.height;
+
+    while (width < targetWidth || iterations > 10) {
+      iterations += 1;
+      dimensions = appContainer.getBoundingClientRect();
       console.log('Width:', dimensions.width);
       console.log('Height:', dimensions.height);
-      width = dimensions.width;
-      height = dimensions.height;
-    } else {
-      console.error('Element with id "main-div" not found');
+      const width = dimensions.width;
+      const height = dimensions.height;
+
+      const scaleFactor = await appWindow.scaleFactor();
+      const currentSize = (await appWindow.innerSize()).toLogical(scaleFactor);
+      const w_diff = currentSize.width - width;
+      const h_diff = currentSize.height - height;
+      if (w_diff > 0 && width > 0 || h_diff > 0 && height > 0) {
+        await handleScaleChange(currentSize.width + w_diff, currentSize.height + h_diff);
+      }
+      console.log('iterations: ', iterations);
+
+
     }
-    
-    fixWindowSize();
+  };
+    // Function handling scale change (DPI)
 
-    // Czyszczenie event listenera po odmontowaniu komponentu
-    return () => {
-    };
-  }, []);
+    useEffect(() => {
 
-  appWindow.setSize(new LogicalSize(715, 505)).then(() => {
-    console.log('resized!');
-  });
-  // appWindow.scaleFactor().then((factor) => {
-  // console.log(`Skalowanie DPI: ${factor}`);
-  // appWindow.setSize(new PhysicalSize(800 * factor, 600 * factor));
-  return (
-    <div id="main-div" className="bg-[#191b28] w-screen h-screen">
-      {/* <div className="border-0 bg-contain bg-no-repeat bg-window_frame z-[9999] w-full"> */}
-      <div className="flex flex-col text-center h-screen w-full">
-        <TopBar />
-        <LoadingProvider>
-          <Banner />
-          <Nav />
-          <PlayButton />
-        </LoadingProvider>
+      setCorrectWindowSize(715);
+
+      // Czyszczenie event listenera po odmontowaniu komponentu
+      return () => {
+      };
+    }, []);
+
+    // appWindow.scaleFactor().then((factor) => {
+    // console.log(`Skalowanie DPI: ${factor}`);
+    // appWindow.setSize(new PhysicalSize(800 * factor, 600 * factor));
+    return (
+      <div id="main-div" className="bg-[#191b28] w-screen h-screen">
+        {/* <div className="border-0 bg-contain bg-no-repeat bg-window_frame z-[9999] w-full"> */}
+        <div className="flex flex-col text-center h-screen w-full">
+          <TopBar />
+          <LoadingProvider>
+            <Banner />
+            <Nav />
+            <PlayButton />
+          </LoadingProvider>
+        </div>
       </div>
-    </div>
-    // </div>
-  );
-}
+      // </div>
+    );
+  }
 
-export default App;
+  export default App;
