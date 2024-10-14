4fc5fb69b6e1ed01688cab13ecb29c9cf9d00338
daedel <dgasiorowskii@gmail.com>
2024-10-14 15:21:56 +0200

    react on scale change

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,31 @@ import { LoadingProvider } from "./contexts/Loading";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import TopBar from "./components/TopBar";
 import { appWindow, LogicalSize } from '@tauri-apps/api/window';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
-  
+
+  useEffect(() => {
+    // Funkcja obsługująca zmianę skali (DPI)
+    const handleScaleChange = async () => {
+      // Zmiana rozmiaru okna na 715x505 po zmianie skali
+      await appWindow.setSize(new LogicalSize(715,  505 ));
+    };
+
+    // Nasłuchiwanie na zdarzenie zmiany skali
+    const unlisten = appWindow.onScaleChanged(({ payload }) => {
+      console.log(`Skala zmieniona na: ${payload}`);
+      handleScaleChange();
+    });
+
+    // Czyszczenie event listenera po odmontowaniu komponentu
+    return () => {
+      unlisten.then((fn) => fn());
+    };
+  }, []);
+
   appWindow.setSize(new LogicalSize(715, 505)).then(() => {
     console.log('resized!');
   });
@@ -23,15 +43,15 @@ function App() {
   return (
     <div className="bg-[#191b28] w-screen h-screen">
       {/* <div className="border-0 bg-contain bg-no-repeat bg-window_frame z-[9999] w-full"> */}
-        <div className="flex flex-col text-center h-screen w-full">
-          <TopBar />
-          <LoadingProvider>
-            <Banner />
-            <Nav />
-            <PlayButton />
-          </LoadingProvider>
-        </div>
+      <div className="flex flex-col text-center h-screen w-full">
+        <TopBar />
+        <LoadingProvider>
+          <Banner />
+          <Nav />
+          <PlayButton />
+        </LoadingProvider>
       </div>
+    </div>
     // </div>
   );
 }
